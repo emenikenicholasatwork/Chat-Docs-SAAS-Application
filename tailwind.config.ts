a952ccac346afb0426dacfcd1f69a0c32bfb7715
emenikenicholasatwork <nicholasemenikeatwork@gmail.com>
2024-08-29 21:24:13 +0100

    making commit on the tailwindcss configh file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,71 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        '2xl': '1400px',
       },
+    },
+    extend: {
+      maxWidth: {
+        "8xl": "1408px"
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary)-forground)"
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--primary)-secondary)"
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--primary)-destructive)"
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--primary)-muted)"
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--primary)-accent)"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--primary)-secondary)"
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--primary)-card)"
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "cal(var(--radius) - 2px)",
+        sm: "cal(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "accordion-down": "accordion-down 0.2s ease-out",
+      }
     },
   },
   plugins: [],
