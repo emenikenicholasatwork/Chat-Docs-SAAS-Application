bc303695895eb2fcd2acc95b18c6ee2609ee9016
emenikenicholasatwork <nicholasemenikeatwork@gmail.com>
2024-09-04 18:13:14 +0100

    commit code to cloud

---
+++ src/lib/authenticators.ts
@@ -0,0 +1,13 @@
+
+
+export async function encrypt(payload: any) {
+    return await new SignJ
+}
+
+export async function decrypt(input: string): Promise<any> {
+    const { payload }
+}
+
+export async function login() {
+
+}
